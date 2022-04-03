a8985e1b6d6792754d078a41e786bbfd0fdcc6e7
HITLADBALVANTRAI <101315912+HITLADBALVANTRAI@users.noreply.github.com>
2022-04-03 11:08:39 +0530

    Add files via upload

---
+++ class project.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+Marks = [60, 65, 70, 50, 75, 80, 95, 85, 100, 90, 80, 40, 50, 75, 75, 95, 70, 99, 50, 75]
+
+plt.hist(Marks)
+plt.title("frequency count of marks obtain")
+plt.xlabel('Marks')
+plt.ylabel('Count')
+
+
+
+plt.show()
+
